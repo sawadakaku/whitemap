afc0cd07fb146173d675a1c826e060e947143ed0
SAWADA <pure.defy@gmail.com>
2016-02-28 15:18:53 +0900

    index.htmlを作成，ajaxの実装

--- app_whitemap/main.py
+++ app_whitemap/main.py
@@ -5,6 +5,22 @@ class MainPage(webapp2.RequestHandler):
         self.response.handlers['Content-Type'] = 'text/plain'
         self.response.handlers('Hello, World!')
 
+class UploadHandler(webapp2.RequestHandler):
+    def post(self):
+        pass
+
+class WhitemapHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+class UpdateHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
 app = webapp2.WSGIApplication([
+    ('/upload', UploadHandler),
+    ('/img-whitemap', WhitemapHandler),
+    ('/update', UpdateHandler),
     ('/', MainPage),
 ],debug=True)
+
