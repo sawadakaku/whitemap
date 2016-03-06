cc43ef5c8a3b2116ed4445f28928c8828d9076d6
SAWADA <pure.defy@gmail.com>
2016-03-06 13:41:22 +0900

    アップロードされたファイルをテーブルで表示する。cronの設定

--- app_whitemap/modules/mydb.py
+++ app_whitemap/modules/mydb.py
@@ -3,3 +3,4 @@ from google.appengine.ext import ndb
 class Route(ndb.Model):
     title = ndb.StringProperty(indexed=False)
     kml = ndb.TextProperty()
+    time = ndb.DateTimeProperty(auto_now_add=True)
